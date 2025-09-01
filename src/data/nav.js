4ba081fb0411bf8bc4ebf7521702c52580e25af0
awaisqazi <awais.a.qazi@gmail.com>
2025-09-01 05:31:03 -0500

    Fixed the links issues

---
+++ src/data/nav.js
@@ -0,0 +1,8 @@
+// src/data/nav.js
+export const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About Us", path: "/about" },
+  { name: "Classes", path: "/classes" },
+  { name: "Events", path: "/events" },
+  { name: "Contact", path: "/contact" },
+];
