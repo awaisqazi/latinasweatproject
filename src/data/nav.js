31e2194cab2b84f96f9efede1a95721eda369615
awaisqazi <aaq6@njit.edu>
2025-09-13 18:58:36 -0500

    Hiding events in mobile nav

--- src/data/nav.js
+++ src/data/nav.js
@@ -3,6 +3,6 @@ export const navLinks = [
   { name: "Home", path: "/" },
   { name: "About Us", path: "/about" },
   { name: "Classes", path: "/classes" },
-  { name: "Events", path: "/events" },
+  // { name: "Events", path: "/events" },
   { name: "Contact", path: "/contact" },
 ];
